80d289ced78f0229904e8b67e64420ac4a1fe56d
Cole Marsteller <cole@colemars.dev>
2021-07-15 18:33:28 -0500

    add

--- e2e/App.test.ts
+++ e2e/App.test.ts
@@ -1,7 +1,8 @@
 import { test } from './baseFixtures';
+import { testUrl } from './constants';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:3000")
+  await page.goto(testUrl)
 })
 
 test('use Turquoise as a default background color', async ({ page }) => {
---
+++ e2e/constants.ts
@@ -0,0 +1,1 @@
+export const testUrl = process.env.REACT_APP_TESTING_URL || 'http://localhost:3000/';
