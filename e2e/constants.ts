02a986cfc881e4e43895ed064fc6d4aa178132d8
Cole Marsteller <cole@colemars.dev>
2021-07-20 10:07:16 -0500

    update

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,1 +1,1 @@
-export const testUrl = process.env.REACT_APP_TESTING_URL || 'http://localhost:5000/';
+export const testUrl = process.env.REACT_APP_TESTING_URL || 'http://pipeline-testing-deploy.s3-website-us-west-2.amazonaws.com/';
