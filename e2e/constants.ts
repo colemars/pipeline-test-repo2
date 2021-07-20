0cc8db0665cea98ec12ff34c6b3e40e06da31154
Cole Marsteller <cole@colemars.dev>
2021-07-20 11:53:28 -0500

    hmm

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,1 +1,3 @@
-export const testUrl = process.env.REACT_APP_TESTING_URL || 'http://pipeline-testing-deploy.s3-website-us-west-2.amazonaws.com/';
+// export const testUrl = process.env.REACT_APP_TESTING_URL || 'http://pipeline-testing-deploy.s3-website-us-west-2.amazonaws.com/';
+
+export const testUrl = process.env.REACT_APP_TESTING_URL || 'http://localhost:5001/';
